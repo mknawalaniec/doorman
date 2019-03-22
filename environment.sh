cdf886c8f2f17acb36488b44c2df60f5c20aa735
EC2 Default User <ec2-user@ip-172-31-22-111.ec2.internal>
2019-03-22 15:55:33 +0000

    change python version

--- environment.sh
+++ environment.sh
@@ -1,9 +1,9 @@
 # Custom Configuration
 export AWS_REGION=us-east-1
 export EMAIL_SOURCE=source@email.com
-export SLACK_CHANNEL_ID=[channel name]
-export SLACK_TRAINING_CHANNEL_ID=[channel name]
-export SLACK_API_TOKEN=[oath token]
+export SLACK_CHANNEL_ID=doorman-eyh
+export SLACK_TRAINING_CHANNEL_ID=doorman-eyh
+export SLACK_API_TOKEN=xoxp-2151114050-47016907444-584313133284-66a265f3152ff1b2be6be971e98030c7
 
 # General setup
 export name=sample-deeplens
